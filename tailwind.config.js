ce86dcc8a3064ce7a6a5e39990e3f4310c1b3e17
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-02-18 16:31:04 -0600

    added dynamic url

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,14 @@ export default {
       colors: {
         'primary': '#3662E3',
         'secondary': '#111729',
-        'white': '#F2F5F9',
+        'custom-white': '#F2F5F9',
         'custom-gray': '#364153',
         'light-gray': '#4e80ee33',
+      },
+      screens: {
+        'fold': {
+          max: '320px',
+        }
       }
     },
   },
